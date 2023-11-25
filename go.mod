3c0331f30af4547d2a6baee25781c95965676d7b
Vladislav Mihov <vladislav.mihov@syna.pt>
2023-11-25 13:02:19 +0200

    feat: add links& discord webhook post.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module backend
 
 go 1.19
 
-require github.com/mattn/go-sqlite3 v1.14.18 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.18 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+    "os"
     "database/sql"
 	"encoding/json"
     "log"
     "net/http"
+    "bytes"
 
+    "github.com/joho/godotenv"
     _ "github.com/mattn/go-sqlite3"
 )
 
@@ -18,7 +21,12 @@ var database *sql.DB
 
 func main() {
 	var err error
-    
+
+    err = godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
 	database, err = sql.Open("sqlite3", "./data.db")
     if err != nil {
         log.Fatal("Error opening database: ", err)
@@ -40,8 +48,9 @@ func main() {
 
 	statement.Exec()
     http.HandleFunc("/links", listLinks)
+    http.HandleFunc("/add-link", addLink)
 
-    log.Fatal(http.ListenAndServe(":8081", nil))
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 
@@ -71,4 +80,58 @@ func listLinks(w http.ResponseWriter, r *http.Request) {
 
     w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(links)
+}
+
+
+func addLink(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "POST" {
+        http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+        return
+    }
+
+    if err := r.ParseForm(); err != nil {
+        http.Error(w, "Error parsing form", http.StatusBadRequest)
+        return
+    }
+
+    url := r.FormValue("url")
+    if url == "" {
+        http.Error(w, "URL is required", http.StatusBadRequest)
+        return
+    }
+
+    webhookURL := os.Getenv("DISCORD_WEBHOOK_URL")
+    if webhookURL == "" {
+        log.Println("Discord webhook URL is not set")
+        http.Error(w, "Internal server error", http.StatusInternalServerError)
+        return
+    }
+
+    jsonStr := []byte(`{"content": "[_](` + url + `)", "username": "Synaptic Feed"}`)
+    req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        http.Error(w, "Error sending message to Discord", http.StatusInternalServerError)
+        return
+    }
+    defer resp.Body.Close()
+
+    statement, err := database.Prepare("INSERT INTO links (url) VALUES (?)")
+    if err != nil {
+        http.Error(w, "Error preparing insert statement", http.StatusInternalServerError)
+        return
+    }
+    defer statement.Close()
+
+    _, err = statement.Exec(url)
+    if err != nil {
+        http.Error(w, "Error executing insert statement", http.StatusInternalServerError)
+        return
+    }
+
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(map[string]string{"message": "Link added successfully"})
 }
